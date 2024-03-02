4d0771abda22c9849127fc719e4ed5db419ab129
CoolElectronics <me@coolelectronics.me>
2024-03-02 15:27:35 -0500

    update stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import BareClient, { SetTransport } from "@mercuryworkshop/bare-mux";
+import BareClient, { SetSingletonTransport, SetTransport, registerRemoteListener } from "@mercuryworkshop/bare-mux";
 //@ts-ignore
 import "../node_modules/@mercuryworkshop/alicejs/AliceJS.js";
 import "./index.css";
@@ -69,10 +69,13 @@ document.addEventListener("libcurl_load", () => {
   console.log("libcurl.js ready!");
 });
 
+declare var BareMod: any;
 (async () => {
   document.querySelector("#app")?.appendChild(<App />)
+  SetSingletonTransport(new BareMod.BareClient("http://localhost:8080/bare/"));
+  registerRemoteListener(navigator.serviceWorker.controller!);
   // SetTransport("BareMod.BareClient", "http://localhost:8080/bare/");
-  SetTransport("BareTLS.TLSClient", { wisp: "wss://wisp.mercurywork.shop/", type: "epoxy" });
+  // SetTransport("BareTLS.TLSClient", { wisp: "wss://wisp.mercurywork.shop/", type: "epoxy" });
 
   // let root = ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host;
   // let bcctls = new TLSClient({ mux: "ws://localhost:6001" });
@@ -82,5 +85,4 @@ document.addEventListener("libcurl_load", () => {
   //@ts-ignore
   app.active = true;
 })();
-window.b = BareClient;
-window.a = () => SetTransport("BareTLS.TLSClient", { wsproxy: "wss://wisp.mercurywork.shop/" });;
+
