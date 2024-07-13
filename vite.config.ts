2031773059a90a00761efd6a726d2aaa56300abb
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-13 01:47:29 -0500

    fix vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: parseInt(process.env.PORT!) || 3000,
   },
-  plugins: [
-    // crossOriginIsolation()
-  ]
+  plugins: []
 })
