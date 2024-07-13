07a7b8f14309ad375f9ed33849df6e0f6c2b84e4
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-13 01:16:48 -0500

    make vite port configurable

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,7 @@ import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/wsproxy': {
-        target: 'ws://localhost:6001/',
-        changeOrigin: false,
-        secure: false,
-        ws: true,
-      },
-      '/mux': {
-        target: 'wss://localhost:4000/',
-        changeOrigin: false,
-        secure: false,
-        ws: true,
-      },
-    },
+    port: parseInt(process.env.PORT!) || 3000,
   },
   plugins: [
     // crossOriginIsolation()
