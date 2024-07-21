189540727d3e8cc0f5c9f5e926511648b82c7ee6
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-20 19:21:23 -0500

    update submodules

--- public/uv.config.js
+++ public/uv.config.js
@@ -8,4 +8,26 @@ self.__uv$config = {
     bundle: '/uv/uv.bundle.js',
     config: '/uv.config.js',
     sw: '/uv/uv.sw.js',
+    inject: [{
+        host: "google.com",
+        html: `<script>
+            console.log("injected from uv!")
+            </script>`,
+        injectTo: "head"
+    },
+    {
+        host: "discord.com",
+        html: `<script>
+                async function applyCSS() {
+                    let vencordCSS = await fetch("https://raw.githubusercontent.com/Vencord/builds/main/browser.css")
+                    let cssLink = document.createElement("style");
+                    
+                    cssLink.innerHTML = await vencordCSS.text();
+                    document.head.appendChild(cssLink)
+                }
+                applyCSS();
+                </script>
+                <script src="https://raw.githubusercontent.com/Vencord/builds/main/browser.js"></script>`,
+        injectTo: "head"
+    }]
 };
