d57a590765307e4096688de215fa2b540f5a1bde
Toshit Chawda <r58playz@gmail.com>
2024-07-25 16:44:52 -0700

    Revert "set the codec in uv.config.js to Ultraviolet.codec.plain.encode, as a stopgap before we implement the Error api in UV (if it ever happens)"
    This reverts commit edf83da8942bb52bb902c6c330f696243a2a9825.

--- public/sw.js
+++ public/sw.js
@@ -10,31 +10,30 @@ importScripts('/uv/uv.bundle.js');
 importScripts('/uv.config.js');
 importScripts(__uv$config.sw || '/uv/uv.sw.js');
 
-// Fetch vencord and store the text in a variable
-self.vencordjs = "";
-self.vencordcss = "";
-async function loadVC() {
-    self.vencordjs = await fetch("https://raw.githubusercontent.com/Vencord/builds/main/browser.js").then(response => response.text());
-    self.vencordcss = await fetch("https://raw.githubusercontent.com/Vencord/builds/main/browser.css").then(response => response.text());
-    self.__uv$config.inject = [{
-        host: "google.com",
-        html: `<script>
-            console.log("injected from uv!")
-            </script>`,
-        injectTo: "head"
+self.__uv$config.inject = [
+    {
+    host: "google.com",
+    html: `
+        <script id="uv-injected">console.log("Injected into uv!")</script>
+    `,
+    injectTo: "head",
     },
     {
         host: "discord.com",
-        html: `
-            <script>${vencordjs}</script>
-            <style>${vencordcss}</style>
-        `,
+        html: `<script>
+                async function applyCSS() {
+                    let vencordCSS = await fetch("https://raw.githubusercontent.com/Vencord/builds/main/browser.css")
+                    let cssLink = document.createElement("style");
+                    
+                    cssLink.innerHTML = await vencordCSS.text();
+                    document.head.appendChild(cssLink)
+                }
+                applyCSS();
+                </script>
+                <script src="https://raw.githubusercontent.com/Vencord/builds/main/browser.js"></script>`,
         injectTo: "head"
-    }];
-}
-loadVC();
-
-
+    }
+];
 
 const uv = new UVServiceWorker();
 
